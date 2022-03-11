0f060b44f7d38e677681342be4259f38e19e13ec
Jesus Alarcon <jesus.leo0508@gmail.com>
2022-03-11 09:40:09 -0500

    add: crud for customers

---
+++ src/main/java/pe/com/project1/ms/domain/Customer.java
@@ -0,0 +1,34 @@
+package pe.com.project1.ms.domain;
+
+import java.time.LocalDate;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "customers")
+public class Customer {
+
+	private String id;
+	private String names;
+	private String surname;
+	private String lastname;
+	private String dni;
+	private String email;
+	private LocalDate birthdate;
+	private String type;
+
+	public Customer(Customer customer) {
+
+	}
+
+	public Customer update(Customer customer) {
+		return new Customer(this.id, customer.getNames(), customer.getSurname(), customer.getLastname(),
+				customer.getDni(), customer.getEmail(), customer.getBirthdate(), this.type);
+	}
+}
